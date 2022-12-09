69c36a1552754f9e3eda590a5d37e840260a5ddd
treyduffus <105085785+treyduffus@users.noreply.github.com>
2022-12-09 18:29:45 -0500

    backend updates

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,28 +3,27 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
-    # path for about view
-    path(route='about', view=views.about, name='about'),
-
-    # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
-
-    # path for registration
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='', view=views.get_dealerships, name='index')
+    # path for registration
 
     # path for login
-    path('login/', views.login_request, name='login'),
 
     # path for logout
-    path('logout/', views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
