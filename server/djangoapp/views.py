14c32f021c49b874f93ff8f1e53e4102f6b0c0b0
treyduffus <105085785+treyduffus@users.noreply.github.com>
2022-12-09 18:28:35 -0500

    backend updates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarModel, CarMake, CarDealer, DealerReview, ReviewPost
+from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -15,23 +15,18 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
-
-# Create an `about` view to render a static about page
-
 def about(request):
     context = {}
     if request.method == 'GET':
-        return render(request, "djangoapp/about.html", context)
+        return render(request, 'djangoapp/about.html', context)
 
 
-# Create a `contact` view to return a static contact page
 def contact(request):
     context = {}
     if request.method == 'GET':
-        return render(request, "djangoapp/contact.html", context)
+        return render(request, 'djangoapp/contact.html', context)
 
-# Create a `login_request` view to handle sign in request
+
 def login_request(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -39,21 +34,87 @@ def login_request(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            #messages.success(request, "Login successfully!")
+            messages.success(request, "Login successfully!")
             return redirect('djangoapp:index')
         else:
             messages.warning(request, "Invalid username or password.")
             return redirect("djangoapp:index")
 
 
-# Create a `logout_request` view to handle sign out request
 def logout_request(request):
     print("Log out the user '{}'".format(request.user.username))
     logout(request)
     return redirect('djangoapp:index')
 
+# Create a `get_dealer_details` view to render the reviews of a dealer
 
-# Create a `registration_request` view to handle sign up request
+
+def get_dealer_details(request, id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://us-east.functions.appdomain.cloud/api/v1/web/a8bc2f2e-b421-49c4-a3f7-079e85c889c0/dealership-review/get-dealerships"
+        dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+        context["dealer"] = dealer
+    
+        review_url = "https://us-east.functions.appdomain.cloud/api/v1/web/a8bc2f2e-b421-49c4-a3f7-079e85c889c0/dealership-review/get-reviews"
+        reviews = get_dealer_reviews_from_cf(review_url, id=id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
+
+
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        url = "https://us-east.functions.appdomain.cloud/api/v1/web/a8bc2f2e-b421-49c4-a3f7-079e85c889c0/dealership-review/get-dealerships"
+        dealerships = get_dealers_from_cf(url)
+        context["dealership_list"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
+
+# Create a `add_review` view to submit a review
+
+
+def add_review(request, dealer_id):
+    context = {}
+    dealer_url = "https://us-east.functions.appdomain.cloud/api/v1/web/a8bc2f2e-b421-49c4-a3f7-079e85c889c0/dealership-review/get-dealerships"
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+    context["dealer"] = dealer
+    if request.method == 'GET':
+        # Get cars for the dealer
+        cars = CarModel.objects.all()
+        print(cars)
+        context["cars"] = cars
+        
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            username = request.user.username
+            print(request.POST)
+            payload = dict()
+            car_id = request.POST["car"]
+            car = CarModel.objects.get(pk=car_id)
+            payload["time"] = datetime.utcnow().isoformat()
+            payload["name"] = username
+            payload["dealership"] = id
+            payload["id"] = id
+            payload["review"] = request.POST["content"]
+            payload["purchase"] = False
+            if "purchasecheck" in request.POST:
+                if request.POST["purchasecheck"] == 'on':
+                    payload["purchase"] = True
+            payload["purchase_date"] = request.POST["purchasedate"]
+            payload["car_make"] = car.car_make.name
+            payload["car_model"] = car.name
+            payload["car_year"] = int(car.year.strftime("%Y"))
+
+            new_payload = {}
+            new_payload["review"] = payload
+            review_post_url = "https://us-east.functions.appdomain.cloud/api/v1/web/a8bc2f2e-b421-49c4-a3f7-079e85c889c0/dealership-review/add-review"
+            post_request(review_post_url, new_payload, id=id)
+        return redirect("djangoapp:dealer_details", id=id)
+
+
 def registration_request(request):
     context = {}
     if request.method == 'GET':
@@ -76,20 +137,3 @@ def registration_request(request):
             return redirect("djangoapp:index")
         else:
             return render(request, 'djangoapp/registration.html', context)
-
-
-# Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-
-
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
-
